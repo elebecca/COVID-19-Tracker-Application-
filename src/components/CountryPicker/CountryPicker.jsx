f373ad41fde8a6577396a9506c6e0ca1790b6606
Elena <elena.beccaro@gmail.com>
2020-07-08 17:44:56 +0100

    fixed typo

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,16 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { NativeSelect, FromControl } from '@material-ui/core';
+import { NativeSelect } from '@material-ui/core';
+import { FormControl } from '@material-ui/core';
 
 import styles from './CountryPicker.module.css';
-import { countries } from '../../api';
+import { fetchCountries } from '../../api';
 
 const CountryPicker = () => {
+    const [fetchedCountries, setFetchedCountries] = useState([]);
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setFetchedCountries(await fetchCountries());
+        }
+        fetchAPI()
+    }, [setFetchedCountries]);
+    console.log(fetchedCountries)
+
     return (
-        <FromControl>
-            <NativeSelect className={styles.FromControl}>
+        <FormControl >
+            <NativeSelect className={styles.fromControl}>
                 <option value='global'>Global</option>
             </NativeSelect>
-        </FromControl>
+        </FormControl >
     )
 }
 
